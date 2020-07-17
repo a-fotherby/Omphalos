ed77b0049e72843ea306b0b4c8bc24045ce139f3
Angus Fotherby <angusfotherby@gmail.com>
2020-07-17 10:34:42 +0100

    Added method for generating n input files with randomised primary species concentrations in a given condition, in the range [var_min, var_max].

--- trainingModel/generate_inputs.py
+++ trainingModel/generate_inputs.py
@@ -3,10 +3,12 @@ import numpy as np
 import pandas as pd
 import input_file as ipf
 import file_methods as fm
+import random as rand
+import copy
 
 
 def import_template(file_path):
-    """Import the template import file.
+    """Import the template import file. Returns an input_file object, fully populated with all available keyword blocks.
     
     The template input file will define the mineral/species space of interest,
     as well as the geometry of the system in question. 
@@ -27,6 +29,21 @@ def import_template(file_path):
     
     return template
 
+def create_condition_series(template, condition, number_of_files, var_min, var_max):
+    """Create a dictionary of InputFile objects that have randomised parameters in the range [var_min, var_max] for the specified condition."""
     
+    template.sort_condition_block(condition)
     
+    keys = np.arange(number_of_files)
     
+    file_dict = {}
+    
+    for key in keys:
+        file_dict.update({key: copy.deepcopy(template)})
+    
+    for file in file_dict:
+        for species in file_dict[file].condition_blocks[condition].primary_species.keys():
+            file_dict[file].condition_blocks[condition].primary_species.update({species: round(rand.uniform(var_min, var_max), 5)})
+            
+    return file_dict
+

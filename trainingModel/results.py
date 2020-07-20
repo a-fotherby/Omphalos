07cd27dedf070d07ebeaab1b53edf7d9dec0a034
Angus Fotherby <angusfotherby@gmail.com>
2020-07-20 16:14:31 +0100

    Fixed some syntax errors in the get_output method and the class definition.

--- trainingModel/results.py
+++ trainingModel/results.py
@@ -3,7 +3,7 @@ import pandas as pd
 import file_methods as fm
 
 
-class Results(self):
+class Results():
     """An object containing the results of a given CrunchTope input file.
 
     Results objects contain objects refering to the contents of each type of CrunchTope output file,
@@ -20,5 +20,5 @@ class Results(self):
         Thus, the convention for this project is to only specify the final time step in the input file.
         This is justified because we are only interested in the final state of the system in this project.
         """
-        df = fm.read_tec_files(path_to_directory, output)
-        self.results_dict.update{output: df}
+        df = fm.read_tec_file(path_to_directory, output)
+        self.results_dict.update({output: df})

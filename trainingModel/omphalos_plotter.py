a352b52d1c51b89c87054ddccd3d958ba9423fc0
Angus Fotherby <angusfotherby@gmail.com>
2020-08-11 13:42:41 +0100

    Added file for plotting functions specific to Omphalos data sets.

---
+++ trainingModel/omphalos_plotter.py
@@ -0,0 +1,18 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def scatter_attributes(df, *, attributes=[], labels=[], hue=None):
+    """Plots a reduced Seaborn PairGrid scatter plot of attributes against the labels.
+    
+    Arguments:
+    df -- DataFrame containing the attributes and labels in tidy format.
+    attributes -- List of strings identifying attribute columns in df.
+    labels -- List of strings identifying label columns in df.
+    """
+    
+    g = sns.PairGrid(df, x_vars=attributes, y_vars=labels, hue=hue, palette="Blues")
+    g = g.map(plt.scatter)
+    
+    return g

66a39dedd54fe9d9de6bf1cde9bb747267bc4d82
Angus Fotherby <angusfotherby@gmail.com>
2020-09-03 13:17:37 +0100

    Updated get_secondary_porosity to work for any CrunchTope geometry. It now returns a multi-indexed, corresponding to the input file at the top level, and then the spatial secondary precipitation data stored in a tidy format at the next level down.

--- trainingModel/labels.py
+++ trainingModel/labels.py
@@ -19,28 +19,112 @@ def raw_labels(data_set, output):
     return labels
 
 
-def secondary_precip(data_set, condition):
-    """Calculate the total mineral volume evolution over the run.
+def secondary_precip(data_set):
+    """Calculate the total mineral volume evolution over the run by comparing the initial conditions to the final mineral volume output.
 
-    Currently only able to handle a single, uniform geochemical condition for the entire system.
     Keyword arguments:
-    condition -- the dictionary entry key for the condition in question.
+    data_set -- The data set to calculate the secondary preciptation for.
     """
+    
+    # Get DataFrame of output volumes.
+    final_vols = lbls.raw_labels(data_set, 'volume')
+        
+    # Create a new DataFrame with the same geometry as the labels by making a deep copy of the coordinate data.
+    initial_vols = pd.DataFrame()
+    initial_vols = final_vols[['X', 'Y', 'Z']].copy() 
+    
     secondary_precip = pd.DataFrame()
     mineral_vol_init = pd.DataFrame()
     # Ensure that all the condition blocks have been sorted.
     for i, file in enumerate(data_set):
-        data_set[file].check_condition_sort(condition)
+        
+        # Initialise discretization array as CrunchTope defaults.
+        # Could probably move this to be the default when input files are being read in/generated but will leave here for now.
+        disc = [[1, 1], [1, 1], [1,1]]
+        
+        try:
+            # We ensure discretization data is read in as floats.
+            disc[0] = [float(i) for i in test_set[0].keyword_blocks['DISCRETIZATION'].contents['xzones']]
+            disc[1] = [float(i) for i in test_set[0].keyword_blocks['DISCRETIZATION'].contents['yzones']]
+            disc[2] = [float(i) for i in test_set[0].keyword_blocks['DISCRETIZATION'].contents['zzones']]
+        except KeyError as error:
+            print("The discretization in {} has not been specified.\nIf this is in error, check your input file.".format(error.args[0]))
+            
+        row_count = int(disc[0][0] * disc[1][0] * disc[2][0])
+            
+        # Initialise output volume np.array and get the condition volume fractions.
 
-        # Get list of all minerals in the system and record the starting volume
-        # fraction for each.
-        mineral_dict = data_set[file].condition_blocks[condition].minerals
-        for key in mineral_dict:
-            mineral_dict.update({key: mineral_dict[key][0]})
+        output_vols = np.zeros((row_count, len(data_set[file].keyword_blocks['MINERALS'].contents) - 1))    
+        
+        # Construct an initial volume fraction field using initial conditions and the region attribute.
+        for condition in data_set[file].condition_blocks:
+            print(condition)
+            data_set[file].check_condition_sort(condition)
 
-        file_volumes = pd.DataFrame(mineral_dict, index=[i], dtype=float)
-        mineral_vol_init = mineral_vol_init.append(file_volumes)
+            # Get list of all minerals in the system and record the starting volume
+            # fraction for each.
+            mineral_dict = {}
 
-    mineral_vol_out = raw_labels(data_set, 'volume')
-    secondary_precip = mineral_vol_init - mineral_vol_out
+            for key in data_set[file].condition_blocks[condition].minerals:
+                mineral_dict.update({key: data_set[file].condition_blocks[condition].minerals[key][0]})
+
+
+            # Now initialise np.array() based on geometry of system contained in discretization info.
+            # CT cycles through coordinates x -> y -> z in TecPlot output format so we need an array with xboxes * yboxes * zboxes number of rows and minerals number of columns.
+
+            # Following on from this, any coord has a row number = [x + (y * x_len) + (z * x_len * y_len)] in this scheme. (Where coord counting starts from (0, 0, 0))
+            # Contiguous areas in real space are not necissarily contiguous in the row format.
+
+
+            condition_vols = np.fromiter(mineral_dict.values(), dtype=float)
+
+            # Need to get row numbers for each condition and each region specified to start with that condition.
+            # (Still need to amend file reading so that it can correctly interperate non-contiguous condition regions)
+            # Recall that range_set refers to block numbers not coordinates and that counting starts from 1, so must be adjusted at start, but not the end becuase np.arange doesn't include the final number.
+            # Lots of fence-post errors to keep track of here.
+            
+            range_set_list = data_set[file].condition_blocks[condition].region
+
+            for range_set in range_set_list:
+
+                print(range_set)
+
+                if range_set == [[0,0], [0,0], [0,0]]:
+                    print("Unused condition '{}' detected. Skipping.".format(condition))
+                    pass
+                else:
+                    x_rows = np.arange((range_set[0][0]-1), range_set[0][1])
+                    y_rows = np.arange((range_set[1][0]-1), range_set[1][1])
+                    z_rows = np.arange((range_set[2][0]-1), range_set[2][1])
+
+                    index_list_len = len(x_rows) * len(y_rows) * len(z_rows)
+                    print(index_list_len)
+                    row_list = [0] * index_list_len
+
+                    # Get a list of the row indicies corresponding to the region where the condition is applied.
+                    n = 0
+                    for z in z_rows:
+                        for y in y_rows:
+                            for x in x_rows:
+                                row_list[n] = int(x + (y * len(x_rows)) + (z * len(y_rows) * len(z_rows)))
+                                n = n + 1
+
+                    print(row_list)
+                    for row in row_list:
+                        output_vols[row] = condition_vols
+            
+        initial_vol_df = pd.DataFrame(output_vols)
+        initial_vol_df.columns = mineral_dict.keys()
+        initial_vols = initial_vols.reindex_like(final_vols)
+        initial_vols.loc[file].update(initial_vol_df)
+    
+    
+    display(initial_vols)
+    display(final_vols)
+    secondary_precip = initial_vols - final_vols
+    # Recapture coordinate values, as they are destroyed by subtraction.
+    # Somewhat easiers to code than indexing the right columns for subtraction.
+    # Could be a source of issues but I doubt it.
+    secondary_precip[['X', 'Y', 'Z']]= final_vols[['X', 'Y', 'Z']].copy() 
+
     return secondary_precip

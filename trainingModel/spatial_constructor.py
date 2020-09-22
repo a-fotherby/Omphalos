a775483c540e67c8187391f64dafe4839a633718
Angus Fotherby <angusfotherby@gmail.com>
2020-09-22 12:20:03 +0100

    Bug fix; now loop through primary_species value dict and assign manually to stop values being read as arrays of length 1, which causes dtype errors downstream.

--- trainingModel/spatial_constructor.py
+++ trainingModel/spatial_constructor.py
@@ -17,7 +17,9 @@ def populate_array(input_file, primary_species=True, minerals=True):
         mineral_dict = {}
         
         if primary_species:
-            primary_species_dict = input_file.condition_blocks[condition].primary_species
+            # Have to iterate and update dict to stop values being read as single element arrays.
+            for key in input_file.condition_blocks[condition].primary_species:
+                primary_species_dict.update({key: input_file.condition_blocks[condition].primary_species[key][0]})
 
         if minerals:
             # Get list of all minerals in the system and record the starting volume fraction for each. 

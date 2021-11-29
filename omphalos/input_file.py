4ea9919a1064933bdd4544383c9e277473a30d33
a-fotherby <af606@cam.ac.uk>
2021-11-29 14:04:21 +0000

    Add properties for reading in NameList files. Reformat some code.

--- omphalos/input_file.py
+++ omphalos/input_file.py
@@ -5,6 +5,7 @@ import pandas as pd
 import copy
 import re
 
+
 class InputFile:
     """Highest level object, representing a single CrunchTope input file."""
 
@@ -12,9 +13,10 @@ class InputFile:
         self.path = file_path
         self.raw = fm.import_file(self.path)
         self.keyword_blocks = {}
-        self.condition_blocks = {}    
+        self.condition_blocks = {}
+        self.catabolic_pathways = {}
+        self.aqueous_database = {}
         self.timeout = False
-
 
     def get_keyword_block(self, keyword):
         """Method to get a keyword block from the input file, specified by keyword.
@@ -29,7 +31,8 @@ class InputFile:
         Each entry of the list is a single word from the input file line, split by whitespace.
 
         This method works for all keyword blocks except conditions, of which there may be multiple in an input file.
-        In the event that a keyword block is erroneously added more than once in the input file, it will use the first instance of that keyword for assignment.
+        In the event that a keyword block is erroneously added more than once in the input file, it will use the
+        first instance of that keyword for assignment.
         """
         # Get all instances of the keyword in question, in a numpy array.
         block_start = fm.search_input_file(self.raw, keyword)
@@ -56,17 +59,20 @@ class InputFile:
                     keyword_dict.update({line_list[0]: line_list[1:]})
                 except BaseException:
                     print(
-                        'BaseException: This is normally due to a commented line in the input file. If it is not, something has gone really wrong!')
+                        'BaseException: This is normally due to a commented line in the input file. If it is not, '
+                        'something has gone really wrong!')
                 block.contents = keyword_dict
                 self.keyword_blocks.update({keyword: block})
         except IndexError:
-            print('The keyword "{}" you searched for does not exist. If you are sure that this keyword is in your input file, check your spelling.'.format(keyword))
+            print(
+                'The keyword "{}" you searched for does not exist. If you are sure that this keyword is in your input '
+                'file, check your spelling.'.format(keyword))
 
     def get_condition_blocks(self):
         """Special method for getting all CONDITION blocks from an input file, of which there may be multiple.
 
         Assigns each CONDITION block to a dictionary in the inputFile object specifically for geochemical conditions.
-        The key for each dictionary entry is the condition name specified in the CunchTope input file.
+        The key for each dictionary entry is the condition name specified in the CrunchTope input file.
         """
         # Get all instances of the keyword in question, in a numpy array.
         block_start = fm.search_input_file(self.raw, 'CONDITION')
@@ -97,16 +103,16 @@ class InputFile:
 
             condition.contents = keyword_dict
             self.condition_blocks.update({condition_name: condition})
-        
+
         # Get regions for each condition block.
         self.condition_regions()
-            
 
     def get_isotope_block(self):
         """Method to get the isotope block from the input file and encode it as a KeywordBlock object in the InputFile.
 
-        We have to do this in a seperate method because the isotope block is unique in CrunchTope because it has non-unique left-most words (either 'primary' or 'mineral').
-        This means that the dictionary keys keep overwriting each other, so we use the rare mineral entry as the dict key instead.
+        We have to do this in a separate method because the isotope block is unique in CrunchTope because it has
+        non-unique left-most words (either 'primary' or 'mineral'). This means that the dictionary keys keep
+        overwriting each other, so we use the rare mineral entry as the dict key instead.
         """
         # Get all instances of the keyword in question, in a numpy array.
         keyword = 'ISOTOPES'
@@ -125,10 +131,10 @@ class InputFile:
         keyword_dict = {}
         try:
             for a in np.arange(block_start[0], block_end[0]):
-                # Split the line into a list, using whitespace as the delimiter, and use the second left most word as the dict key (in this specific context, the rare isotope)
-                # Commented lines are removed but line number index is preserved.
-                # So put in try-except statement to ignore error thrown by missing
-                # line removed due to commenting.
+                # Split the line into a list, using whitespace as the delimiter, and use the second left most word as
+                # the dict key (in this specific context, the rare isotope) Commented lines are removed but line
+                # number index is preserved. So put in try-except statement to ignore error thrown by missing line
+                # removed due to commenting.
                 try:
                     line_list = self.raw[a].split()
                     reordered_list = [line_list[0]] + line_list[2:]
@@ -140,18 +146,22 @@ class InputFile:
                     keyword_dict.update({line_list[0]: line_list[1:]})
                 except BaseException:
                     print(
-                        'BaseException: this is normally due to a commented line in the input file. If it is not, something has gone really wrong!')
+                        'BaseException: this is normally due to a commented line in the input file. If it is not, '
+                        'something has gone really wrong!')
                 block.contents = keyword_dict
                 self.keyword_blocks.update({keyword: block})
         except IndexError:
-            print('The keyword "ISOTOPES" you searched for does not exist. If you are sure that this keyword is in your input file, check your spelling.')
-
+            print(
+                'The keyword "ISOTOPES" you searched for does not exist. If you are sure that this keyword is in your '
+                'input file, check your spelling.')
 
     def get_initial_conditions_block(self):
-        """Method to get the initial conditions block from the input file and encode it as a KeywordBlock object in the InputFile.
+        """Method to get the initial conditions block from the input file and encode it as a KeywordBlock object in
+        the InputFile.
 
-        We have to do this in a seperate method because the initial conditions block is unique in CrunchTope as conditions can be repeated to form non-contiguous regions, so the left most word is not always unique.
-        This means that the dictionary keys can overwriting each other.
+        We have to do this in a separate method because the initial conditions block is unique in CrunchTope as
+        conditions can be repeated to form non-contiguous regions, so the left most word is not always unique. This
+        means that the dictionary keys can overwriting each other.
         """
         # Get all instances of the keyword in question, in a numpy array.
         keyword = 'INITIAL_CONDITIONS'
@@ -170,13 +180,13 @@ class InputFile:
         keyword_dict = {}
         try:
             for a in np.arange(block_start[0], block_end[0]):
-                # Split the line into a list, using whitespace as the delimiter, and use the second left most word as the dict key (in this specific context, the rare isotope)
-                # Commented lines are removed but line number index is preserved.
-                # So put in try-except statement to ignore error thrown by missing
-                # line removed due to commenting.
+                # Split the line into a list, using whitespace as the delimiter, and use the second left most word as
+                # the dict key (in this specific context, the rare isotope) Commented lines are removed but line
+                # number index is preserved. So put in try-except statement to ignore error thrown by missing line
+                # removed due to commenting.
                 try:
                     line_list = self.raw[a].split()
-                    # Regex extracts keys as unqiue coordinate sets.
+                    # Regex extracts keys as unique coordinate sets.
                     key = re.findall("\d+-\d+", self.raw[a])
                     key = ' '.join(key)
                     # Check to see if the fix keyword has been invoked.
@@ -192,15 +202,16 @@ class InputFile:
                     keyword_dict.update({line_list[0]: line_list[1:]})
                 except BaseException:
                     print(
-                        'BaseException: this is normally due to a commented line in the input file. If it is not, something has gone really wrong!')
+                        'BaseException: this is normally due to a commented line in the input file. If it is not, '
+                        'something has gone really wrong!')
                 block.contents = keyword_dict
                 self.keyword_blocks.update({keyword: block})
         except IndexError:
             print('The keyword "INITIAL_CONDITIONS" you searched for does not exist; check your input file for errors.')
-            
-            
+
     def sort_condition_block(self, condition):
-        """Sort a conditon block dictionary into dictionaries for each types of species (mineral, gas, aqueous, parameter).
+        """Sort a condition block dictionary into dictionaries for each types of species (mineral, gas, aqueous,
+        parameter).
 
         This is required when you need to distinguish between types of entry in a condition block.
         """
@@ -212,8 +223,10 @@ class InputFile:
             primary_species_list = self.keyword_blocks['PRIMARY_SPECIES'].contents.keys(
             )
         except IndexError:
-            print("You must populate your MINERAL, GASES, and PRIMARY_SPECIES keyword blocks before you can sort a condition block.\nTry running the get_keyword_blocks() method first.")
-        # For each entry in the dictionary, compare with the PRIMARY_SPECIES,
+            print(
+                "You must populate your MINERAL, GASES, and PRIMARY_SPECIES keyword blocks before you can sort a "
+                "condition block.\nTry running the get_keyword_blocks() method first.")
+            # For each entry in the dictionary, compare with the PRIMARY_SPECIES,
         # MINERALS, and GASES blocks to assign the entry to the right dict.
         contents = self.condition_blocks[condition].contents
         # Maybe there is a way to make this if logic compact? Worth thinking
@@ -240,7 +253,7 @@ class InputFile:
             # Print out each keyword block, not condition blocks: they require
             # special treatment.
             for block in self.keyword_blocks:
-                # Special treatment for the ISOTOPE block becuase of the way the dictionary is indexed.
+                # Special treatment for the ISOTOPE block because of the way the dictionary is indexed.
                 # Ensure that the dictionary is unpacked in the right order so
                 # that the file has the right syntax.
                 if (block == 'ISOTOPES') or (block == 'INITIAL_CONDITIONS'):
@@ -265,7 +278,7 @@ class InputFile:
                 # this was overwriting data from gi.create_condition_series
                 # because the original template is still stored in
                 # ConditionBlock.contents.
-                if bool(self.condition_blocks[block].parameters) == False:
+                if not bool(self.condition_blocks[block].parameters):
                     self.sort_condition_block(block)
                 else:
                     pass
@@ -276,10 +289,10 @@ class InputFile:
                     self.condition_blocks[block].minerals,
                 ]:
                     for entry in species_type:
-                        # Ugh, weird workaround because of various type error - need to be a string to compose the line, but I want to store as number for data analysis purposes.
-                        # This might come back to bite later, so if things
-                        # start going tits up maybe check here first for any
-                        # type-casting fuckery.
+                        # Ugh, weird workaround because of various type error - need to be a string to compose the
+                        # line, but I want to store as number for data analysis purposes. This might come back to
+                        # bite later, so if things start going tits up maybe check here first for any type-casting
+                        # fuckery.
                         line = copy.deepcopy(species_type[entry])
                         string = entry
                         for word in line:
@@ -290,7 +303,7 @@ class InputFile:
 
     def check_condition_sort(self, condition):
         """Check to see if the condition block has been sorted. If not, sort it."""
-        if bool(self.condition_blocks[condition].parameters) == False:
+        if not bool(self.condition_blocks[condition].parameters):
             self.sort_condition_block(condition)
         else:
             pass
@@ -302,7 +315,7 @@ class InputFile:
         Keyword arguments:
         condition -- the dictionary entry key for the condition in question.
         """
-        if bool(self.condition_blocks[condition].parameters) == False:
+        if not bool(self.condition_blocks[condition].parameters):
             self.sort_condition_block(condition)
         else:
             pass
@@ -315,32 +328,34 @@ class InputFile:
         delta_mineral_vol = mineral_vol_init - mineral_vol_out
 
         return delta_mineral_vol
-    
+
     def condition_regions(self):
-        """Find the coordinates over which condition is initially applied and assign them to the region attribute of the ConditionBlock object.
+        """Find the coordinates over which condition is initially applied and assign them to the region attribute of
+        the ConditionBlock object.
         
-        Condition region is an ordered array of arrays, corresponding to the range over which that condition is applied in X, Y, and Z.
+        Condition region is an ordered array of arrays, corresponding to the range over which that condition is
+        applied in X, Y, and Z.
         """
         # Initialise all the region attributes to prevent infinitely appending lists.
         for condition in self.condition_blocks:
-            self.condition_blocks[condition].region=[]
-        
+            self.condition_blocks[condition].region = []
+
         for coord_string in self.keyword_blocks['INITIAL_CONDITIONS'].contents:
             # Skip the block title line.
             if not coord_string:
                 pass
             else:
                 condition = self.keyword_blocks['INITIAL_CONDITIONS'].contents[coord_string][0]
-                condition_region = [[1,1],[1,1],[1,1]]
+                condition_region = [[1, 1], [1, 1], [1, 1]]
                 try:
                     coord_pairs = coord_string.split()
                     for i, coords in enumerate(coord_pairs):
                         result = re.findall("\d+", coords)
                         result = list(map(int, result))
                         condition_region[i] = result
-                        
+
                 except KeyError:
-                    condition_region = [[0,0], [0,0], [0,0]]
+                    condition_region = [[0, 0], [0, 0], [0, 0]]
                     print("Warning: The condition {} was not specified as a initial condition".format(condition))
-                
+
                 self.condition_blocks[condition].region.append(condition_region)

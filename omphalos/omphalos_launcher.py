78966a4acbc18c542acfc235de928540e035cda1
a-fotherby <af606@cam.ac.uk>
2021-11-29 14:31:14 +0000

    cleanup and protecting main thread

--- omphalos/omphalos_launcher.py
+++ omphalos/omphalos_launcher.py
@@ -1,23 +1,17 @@
 from context import omphalos
 
-import numpy as np
-import matplotlib as pyplot
-import omphalos.file_methods as fm
-import omphalos.input_file as ipf
-import omphalos.generate_inputs as gi
-import omphalos.run
-import sys
-import subprocess
-import pickle
+import file_methods as fm
+import generate_inputs as gi
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('path_to_config', type=str, help='YAML file containing options.')
-parser.add_argument('output_name', type=str, help='Output file name.')
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_config', type=str, help='YAML file containing options.')
+    parser.add_argument('output_name', type=str, help='Output file name.')
+    args = parser.parse_args()
 
-dataset = gi.make_dataset(args.path_to_config)
+    dataset = gi.make_dataset(args.path_to_config)
 
-# Pickle the data.
-fm.pickle_data_set(dataset, f'{args.output_name}')
+    # Pickle the data.
+    fm.pickle_data_set(dataset, f'{args.output_name}')
 

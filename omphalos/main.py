fc2ec59f5410780ff93dd27b74e6755f7d64fe36
a-fotherby <af606@cam.ac.uk>
2021-12-06 15:48:26 +0000

    refactor main thread

--- omphalos/main.py
+++ omphalos/main.py
@@ -1,10 +1,10 @@
 from context import omphalos
 
-import file_methods as fm
-import generate_inputs as gi
-import argparse
-
 if __name__ == '__main__':
+    import file_methods as fm
+    import generate_inputs as gi
+    import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('path_to_config', type=str, help='YAML file containing options.')
     parser.add_argument('output_name', type=str, help='Output file name.')
@@ -14,4 +14,3 @@ if __name__ == '__main__':
 
     # Pickle the data.
     fm.pickle_data_set(dataset, f'{args.output_name}')
-

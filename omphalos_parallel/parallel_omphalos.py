1e0a0dcfc172b538f9baa0d03d6e064c6ad14f04
a-fotherby <af606@cam.ac.uk>
2021-12-15 16:27:00 +0000

    updates to make compatible with refactored code

--- omphalos_parallel/parallel_omphalos.py
+++ omphalos_parallel/parallel_omphalos.py
@@ -6,14 +6,11 @@ config -- new option, specifies YAML file containing template modification optio
 """
 import argparse
 from context import omphalos
-import omphalos.generate_inputs as gi
-import omphalos.file_methods as fm
+from omphalos.template import Template
 import slurm_interface as si
 import subprocess
 import yaml
 from time import time
-
-import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('path_to_config', type=str, help='YAML file containing options.')
@@ -27,8 +24,8 @@ dir_name = 'run'
 with open(args.path_to_config) as file:
     config = yaml.full_load(file)
 
-template = gi.import_template(config['template'])
-file_dict = gi.configure_input_files(template, config)
+template = Template(config['template'])
+file_dict = template.make_dict()
 
 dict_size = len(file_dict)
 
@@ -37,15 +34,6 @@ t_start = time()
 subprocess.run([f'parallel "mkdir {dir_name}{{1}}" ::: {{0..{dict_size}}}'], shell=True, executable='/bin/bash')
 subprocess.run([f'parallel "cp {config["database"]} {dir_name}{{1}}/{config["database"]}" ::: {{0..{dict_size}}}'], shell=True, executable='/bin/bash')
                 
-if 'aqueous_database' in config:
-    subprocess.run([f'parallel "cp {config["aqueous_database"]} {dir_name}{{1}}/{config["aqueous_database"]}" ::: {{0..{dict_size}}}'], shell=True, executable='/bin/bash')
-
-# Legacy options for old CT input files that require them.
-if 'catabolic_pathways' in config:
-    subprocess.run([f'parallel "cp {config["catabolic_pathways"]} {dir_name}{{1}}/{config["catabolic_pathways"]}" ::: {{0..{dict_size}}}'], shell=True, executable='/bin/bash')
-    subprocess.run([f'parallel "cp CatabolicControl.ant {dir_name}{{1}}/CatabolicControl.ant" ::: {{0..{dict_size}}}'], shell=True, executable='/bin/bash')
-    
-    
 for file in file_dict:
     file_dict[file].path = f'{dir_name}{file}/{file_name_scheme}.in'
     file_dict[file].print_input_file()

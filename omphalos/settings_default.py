27b780ff42dbceafd8a365bc02319351820af3c8
Angus Fotherby <af606@cam.ac.uk>
2022-04-13 15:29:08 +0100

    fix wrong settings.py pushed

--- omphalos/settings_default.py
+++ omphalos/settings_default.py
@@ -1,4 +1,4 @@
 # Put the full paths to your CrunchTope executables and your Omphalos directories in here.
 # Then remove the _default from this file name.
-crunch_dir = '/home/angus/soft/CrunchTope/CrunchTope'
-omphalos_path = '/home/angus/work/Omphalos'
+crunch_dir = '/your/CrunchTope/path'
+omphalos_path = '/your/Omphalos/path'

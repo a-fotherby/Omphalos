f02b9bc81f2d8f99441d772daadc72c7b24d24a0
a-fotherby <af606@cam.ac.uk>
2022-04-22 17:12:26 +0100

    dunder name dunder main

--- rhea/compile_results.py
+++ rhea/compile_results.py
@@ -1,8 +1,9 @@
 import slurm_interface as si
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("dict_len", type=int, help="Length of dictionary to create/compile.")
-args = parser.parse_args()
-
-si.compile_results(args.dict_len)
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dict_len", type=int, help="Length of dictionary to create/compile.")
+    args = parser.parse_args()
+    
+    si.compile_results(args.dict_len)

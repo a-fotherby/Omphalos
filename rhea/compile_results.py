b0128a8fa1251a7f9f6c9d4ad8b317e4c0476a50
a-fotherby <af606@cam.ac.uk>
2022-09-01 13:04:27 +0100

    trim requirements

--- rhea/compile_results.py
+++ rhea/compile_results.py
@@ -1,6 +1,9 @@
 import slurm_interface as si
 import argparse
 
+#import matplotlib
+#matplotlib.use('Agg')
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("dict_len", type=int, help="Length of dictionary to create/compile.")
